c9dfb58d7d1ac0783d4ee072210ad479d9137b1c
sami692139 <samuel.tassew2020@gmail.com>
2025-04-09 00:02:25 +0300

    login page update on if response

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -10,42 +10,42 @@ async function logIn(req, res, next) {
   try {
     console.log(req.body);
     const employeeData = req.body;
-    // Call the logIn method from the login service 
+
     const employee = await loginService.logIn(employeeData);
-    // If the employee is not found
+
     if (employee.status === "fail") {
-      res.status(403).json({
+      return res.status(403).json({
         status: employee.status,
         message: employee.message,
       });
-      // console.log(employee.message);
     }
-    // If successful, send a response to the client
+
     const payload = {
       employee_id: employee.data.employee_id,
       employee_email: employee.data.employee_email,
       employee_role: employee.data.company_role_id,
       employee_first_name: employee.data.employee_first_name,
     };
+
     const token = jwt.sign(payload, jwtSecret, {
       expiresIn: "24h",
     });
-    // console.log(token);
+
     const sendBack = {
       employee_token: token,
     };
-    res.status(200).json({
+
+    return res.status(200).json({
       status: "success",
       message: "Employee logged in successfully",
       data: sendBack,
     });
   } catch (error) {
-    console.error("Login Error:", error); // <--- ADD THIS
-    res.status(500).json({
+    console.error("Login Error:", error);
+    return res.status(500).json({
       status: "error",
       message: "Internal server error",
     });
-
   }
 }
 
